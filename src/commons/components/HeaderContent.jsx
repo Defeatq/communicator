be4f01d8c47db9937e988bf9087b4cda93e6f5f0
Artem <awrer1234eads@gmail.com>
2022-08-22 23:28:16 +0300

    create home page link

--- src/commons/components/HeaderContent.jsx
+++ src/commons/components/HeaderContent.jsx
@@ -1,15 +1,18 @@
 import homePageIcon from '../assets/images/house.png';
 import HeaderLink from './HeaderLink';
 import { PATHS } from '../pages-links-name';
+import { Link } from 'react-router-dom';
 
 function HeaderContent() {
   return (
     <div className="wrapper header__content">
       <nav className="header__navigation">
-        <img className="header__icon" src={ homePageIcon } alt="home" />
+        <Link to="/">
+          <img className="header__icon" src={ homePageIcon } alt="home" />
+        </Link>
         {
           PATHS.map((page, index) => 
-            <HeaderLink key={ index } path={ page.PATH } text={ page.NAME } isLastLink={ PATHS.lenght - 1 === index } />
+            <HeaderLink key={ index } path={ page.path } text={ page.name } isLastLink={ PATHS.lenght - 1 === index } />
           )
         }
       </nav>
