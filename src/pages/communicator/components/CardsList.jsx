4455299495070bc1471c269518524631b6ff3528
Artem <awrer1234eads@gmail.com>
2022-08-26 23:52:39 +0300

    refactoring

--- src/pages/communicator/components/CardsList.jsx
+++ src/pages/communicator/components/CardsList.jsx
@@ -5,15 +5,11 @@ import LOCAL_STORAGE from "../../../store/localStorage";
 function CardsList() {
   const cards = LOCAL_STORAGE.getFavouriteCards();
 
-  useEffect(() => {
-    console.log(cards)
-  }, [cards])
-
   return (
     <section className="cards">
       <div className="wrapper cards__list">
         {
-          cards.map((card, index) =>
+          cards?.map((card, index) =>
             <Card key={ index }
                   name={ card.name }
                   id={ card.id }
