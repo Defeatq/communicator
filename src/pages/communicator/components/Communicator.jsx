73e02d36c431e21a2e370cef759ff53d41a373bb
Artem <awrer1234eads@gmail.com>
2022-08-26 23:53:30 +0300

    create communicator layout

--- src/pages/communicator/components/Communicator.jsx
+++ src/pages/communicator/components/Communicator.jsx
@@ -1,8 +1,19 @@
 function Communicator() {
   return (
-    <div>
-      Communicator
-    </div>
+    <section className="communicator">
+      <div className="wrapper">
+        <div className="communicator__actions">
+          <div className="communicator__cards">
+
+          </div>
+          <div className="communicator__control">
+            <button className="communicator__play">
+              Play
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
