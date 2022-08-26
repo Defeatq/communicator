ee7256723c7bf3c3e198d658d6c9bad674a838b6
Artem <awrer1234eads@gmail.com>
2022-08-27 00:38:14 +0300

    create communicator layout

--- src/pages/communicator/components/Communicator.jsx
+++ src/pages/communicator/components/Communicator.jsx
@@ -1,3 +1,5 @@
+import { FaPlay, FaTrash, FaArrowLeft } from 'react-icons/fa';
+
 function Communicator() {
   return (
     <section className="communicator">
@@ -7,9 +9,17 @@ function Communicator() {
 
           </div>
           <div className="communicator__control">
-            <button className="communicator__play">
-              Play
+            <button className="communicator__play-button">
+              <FaPlay />
             </button>
+            <div className="communicator__delete">
+              <button className="communicator__remove-button">
+                <FaArrowLeft />
+              </button>
+              <button className="communicator__clear-button">
+                <FaTrash />
+              </button>
+            </div>
           </div>
         </div>
       </div>
