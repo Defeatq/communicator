1a828279a2e23049ef917a3acd5f0ea8d897ecdd
Artem <awrer1234eads@gmail.com>
2022-08-30 01:02:52 +0300

    create store provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,21 @@ import Home from './pages/home/components/Home';
 import CardSearchPage from './pages/card-search/components/CardSearchPage';
 import UndefinedPage from './pages/undefined/components/UndefinedPage';
 import CommunicatorPage from './pages/communicator/components/CommunicatorPage';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={ <Home /> } />
-        <Route path='cards' element={ <CardSearchPage /> } />
-        <Route path='communicator' element={ <CommunicatorPage /> } />
-        <Route path='*' element={ <UndefinedPage /> } />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+          <Route path='cards' element={ <CardSearchPage /> } />
+          <Route path='communicator' element={ <CommunicatorPage /> } />
+          <Route path='*' element={ <UndefinedPage /> } />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
