be18ca036295144a1865cf2569e12550f6e10940
Artem <awrer1234eads@gmail.com>
2022-08-30 01:03:30 +0300

    create communicator store actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,10 +3,19 @@ import { createAction } from "@reduxjs/toolkit";
 const ACTIONS = {
   ADD_FAVOURITE: 'ADD_FAVOURITE',
   REMOVE_FAVOURITE: 'REMOVE_FAVOURITE',
+  ADD_COMMUNICATOR_WORD: 'ADD_COMMUNICATOR_WORD',
+  REMOVE_COMMUNICATOR_WORD: 'REMOVE_COMMUNICATOR_WORD',
+  CLEAR_COMMUNICATOR_QUEUE: 'CLEAR_COMMUNICATOR_QUEUE',
 }
 
 const addToFavourites = createAction( ACTIONS.ADD_FAVOURITE );
 
 const removeFromFavourites = createAction( ACTIONS.REMOVE_FAVOURITE );
 
-export { ACTIONS, addToFavourites, removeFromFavourites };
+const COMMUNICATOR_ACTION_GENERATORS = {
+  addCommunicatorWord: createAction( ACTIONS.ADD_COMMUNICATOR_WORD ),
+  removeCommunicatorWord: createAction( ACTIONS.REMOVE_COMMUNICATOR_WORD ),
+  clearCommunicatorQueue: createAction( ACTIONS.CLEAR_COMMUNICATOR_QUEUE ),
+}
+
+export { ACTIONS, addToFavourites, removeFromFavourites, COMMUNICATOR_ACTION_GENERATORS };
