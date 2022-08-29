2faee90bb45513b77f1ddda8296c460e60974b5a
Artem <awrer1234eads@gmail.com>
2022-08-29 23:56:31 +0300

    change card layout

--- src/commons/components/Card.jsx
+++ src/commons/components/Card.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { FaCross } from 'react-icons/fa';
 import store from '../../store/store';
 import { addToFavourites, removeFromFavourites } from '../../store/actions';
 import '../assets/styles/card.css';
@@ -6,7 +7,7 @@ import { playSound } from '../audio';
 import LOCAL_STORAGE from '../../store/localStorage';
 
 function Card(props) {
-  const { id, name, image } = props;
+  const { id, name, image, isAddable, onClick } = props;
   const [checked, setChecked] = useState( false );
 
   function handleFavourite() {
@@ -40,11 +41,15 @@ function Card(props) {
   }, [id]);
 
   return (
-    <div className="card">
+    <div className="card" onClick={ onClick }>
       <div className="card__interaction">
         <button className="card__sound" onClick={ () => playSound( name ) } />
         <img className="card__image" src={ image } alt={ name } />
-        <input className="card__favourite" type="checkbox" checked={ checked } onChange={ handleFavourite } />
+        {
+          isAddable ?
+          <></> :
+          <input className="card__favourite" type="checkbox" checked={ checked } onChange={ handleFavourite } />
+        }
       </div>
       <h4 className="card__name">
         { name }
