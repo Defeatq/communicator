78334f63963b89c4c0ee713f7c63d1845067738e
Artem <awrer1234eads@gmail.com>
2022-08-30 01:04:04 +0300

    add actions to reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -3,6 +3,7 @@ import LOCAL_STORAGE from "./localStorage";
 
 const initialState = {
   favourites: LOCAL_STORAGE.getFavouriteCards() || [],
+  communicator: [],
 }
 
 function reduceFavourites(state = initialState, action) {
@@ -15,6 +16,18 @@ function reduceFavourites(state = initialState, action) {
       return Object.assign({}, state, {
         favourites: state.favourites.filter( card => card.id !== action.payload.id ),
       });
+    case ACTIONS.ADD_COMMUNICATOR_WORD:
+      return Object.assign({}, state, {
+        communicator: [...state.communicator, action.payload],
+      })
+    case ACTIONS.REMOVE_COMMUNICATOR_WORD:
+      return Object.assign({}, state, {
+        communicator: state.communicator.slice(0, state.communicator.length - 1),
+      })
+    case ACTIONS.CLEAR_COMMUNICATOR_QUEUE:
+      return Object.assign({}, state, {
+        communicator: [],
+      })
   }
 }
 
