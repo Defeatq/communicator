0ee0f3b2e590bcff66738c212ac011b2adfa1f55
Artem <awrer1234eads@gmail.com>
2022-09-20 23:27:54 +0300

    change outdated router structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/components/Home';
-import CardSearchPage from './pages/card-search/components/CardSearchPage';
-import UndefinedPage from './pages/undefined/components/UndefinedPage';
-import CommunicatorPage from './pages/communicator/components/CommunicatorPage';
 import { Provider } from 'react-redux';
+import PagesRouter from './commons/components/PagesRouter';
 import store from './store/store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={ store }>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={ <Home /> } />
-          <Route path='cards' element={ <CardSearchPage /> } />
-          <Route path='communicator' element={ <CommunicatorPage /> } />
-          <Route path='*' element={ <UndefinedPage /> } />
-        </Routes>
-      </BrowserRouter>
+      <PagesRouter />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
